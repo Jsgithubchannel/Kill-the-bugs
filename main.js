8cd27db5d08f2c5967ca7d7744daabadc487f68c
JS <jsschannel99@gmail.com>
2020-07-19 15:04:30 +0900

    JS: try again

--- main.js
+++ main.js
@@ -7,11 +7,27 @@ const menu = document.querySelector(".menu__container");
 const catch_cnt = document.querySelector(".catch__cnt");
 const carrot_pull = document.querySelector(".carrot_pull");
 const bg_music = document.querySelector(".bg_music");
+const try_again = document.querySelector(".try__again");
+const ctr = document.querySelector(".ctr");
+const lose = document.querySelector(".lose");
+const win = document.querySelector(".win");
+const timer = document.querySelector(".cntdown__timer");
+
 start.addEventListener("click", () => {
   onStart();
   countDownTimer();
   setPosition(carrot);
   setPosition(bug);
+});
+
+try_again.addEventListener("click", () => {
+  reset();
+  onStart();
+  countDownTimer();
+  setPosition(carrot);
+  setPosition(bug);
+  lose.style.visibility = "hidden";
+  win.style.visibility = "hidden";
 });
 
 for (let i = 0; i < carrot.length; i++) {
@@ -22,46 +38,58 @@ for (let i = 0; i < carrot.length; i++) {
   });
 }
 
+function reset() {
+  for (let i = 0; i < carrot.length; i++) {
+    carrot[i].style.visibility = "visible";
+  }
+  catch_cnt.textContent = 0;
+  let timeleft = 10;
+  timer.textContent = `0:${timeleft}`;
+}
+
 function onStart() {
   const imgs = document.querySelector(".img__container");
   imgs.style.visibility = "visible";
   end.style.visibility = "visible";
   start.style.visibility = "hidden";
+  try_again.style.visibility = "hidden";
   bg_music.play();
 }
 
 function countDownTimer() {
-  const timer = document.querySelector(".cntdown__timer");
   const bug_pull = document.querySelector(".bug_pull");
   let timeleft = 10;
 
   let download_timer = setInterval(function () {
-    const ctr = document.querySelector(".ctr");
-    const lose = document.querySelector(".lose");
-    const win = document.querySelector(".win");
     const game_win = document.querySelector(".game_win");
 
     timeleft--;
     timer.textContent = `0:${timeleft}`;
     if (timeleft <= 0 && catch_cnt.textContent != carrot.length) {
       clearInterval(download_timer);
+      win.style.visibility = "hidden";
       ctr.style.visibility = "visible";
       lose.style.visibility = "visible";
+      try_again.style.visibility = "visible";
+      bg_music.pause();
+      bug_pull.play();
     } else if (catch_cnt.textContent == carrot.length) {
       clearInterval(download_timer);
       ctr.style.visibility = "visible";
       lose.style.visibility = "hidden";
       win.style.visibility = "visible";
+      try_again.style.visibility = "visible";
       bg_music.pause();
       // bg_music.currentTime = 0;
       game_win.play();
     }
     for (let i = 0; i < bug.length; i++) {
       bug[i].addEventListener("click", () => {
+        bg_music.pause();
         bug_pull.play();
         clearInterval(download_timer);
-        ctr.style.visibility = "visible";
         lose.style.visibility = "visible";
+        try_again.style.visibility = "visible";
       });
     }
   }, 1000);
